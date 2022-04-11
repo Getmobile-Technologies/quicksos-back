1ebffb14661aed17acc0ad50c83535dd82083334
Nnebue Desmond <46574948+sirdesmond09@users.noreply.github.com>
2022-04-11 09:25:50 +0100

    update model reg

--- report/admin.py
+++ report/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import AssignedCase, Report
 # Register your models here.
 
-admin.site.register(AssignedCase, Report)
+admin.site.register([AssignedCase, Report])
