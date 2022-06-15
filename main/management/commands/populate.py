e8125d5ca6e3442569db712eebd130376474ae19
sirdesmond09 <sirdesmond09@gmail.com>
2022-06-15 11:31:32 +0100

    feature: add admin script to populate data

---
+++ main/management/commands/populate.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...models import Agency, Issue, Question
+
+
+class Command(BaseCommand):
+    help = 'Populates the DB with agencies, issues and questions'
+
+    def handle(self, *args, **options):
+        agencies = [("Nigeria Police Force", "NPF"), ("Lagos State Emergency Management Agency", "LASEMA")]
+        agency = [Agency(name=agent[0], acronym=agent[1]) for agent in agencies]
+        Agency.objects.bulk_create(agency)
+        
+        issues = ["Fight", "Rape", "Dead Body"]
+        
+        Issue.objects.create([Issue(name=issue) for issue in issues])
+        
+        questions = {
+            issues[0] : ["Describe the details of the incident", "Please send us a picture."],
+            issues[1] : ['Describe details of the incident', 
+                         'What is the victim name?', 
+                         'How old is he/she?', 
+                         'What is the victim\'s gender?', 
+                         'Have the victim (he/she) had a bath?', 
+                         'Have the victim reported to any police station?' ],
+            issues[3] : ["Describe the details of the incident", "Please send us a picture."],
+        }
+        
+        
+        for issue in issues:
+            ish = Issue.objects.get(name = issue)
+            Question.objects.bulk_create([Question.objects.create(issue=ish,question = question) for question in questions[issue]])
+            
+        self.stdout.write(self.style.SUCCESS('Successfully added data'))
