320e9cc35c25f1d2cfb3a6c7c190e3540c91bdcc
sirdesmond09 <sirdesmond09@gmail.com>
2022-07-01 15:42:21 +0100

    fix:forget password

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from .serializers import UserSerializer
 from django.dispatch import receiver
 from django_rest_passwordreset.signals import reset_password_token_created
@@ -9,27 +10,27 @@ from rest_framework import serializers
 from django.template.loader import render_to_string
 
 
-domain = 'quicksos.com'
+domain = 'quicksos.vercel.app'
 url='#'
 User = get_user_model()
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-
     token = "https://{}/forgot-password/{}".format(domain,reset_password_token.key)
     
     # msg_html = render_to_string('forgot_password_otp.html', {
     #                     'first_name': str(instance.user.first_name).title(),
     #                     'code':code})
-    
-    message= 'Hello {},\n\nYou are receiving this message because you or someone else have requested the reset of the password for your account.\nKindly follow tis link or copy it to your browser to change your password:\n{}\n\nPlease if you did not request this please ignore this e-mail and your password would remain unchanged.\n\nRegards,\nQuick SOS Support'.format(instance.first_name, token)
+    user = reset_password_token.user
+
+    message= 'Hello {},\n\nYou are receiving this message because you or someone else have requested the reset of the password for your account.\nKindly follow tis link or copy it to your browser to change your password:\n{}\n\nPlease if you did not request this please ignore this e-mail and your password would remain unchanged.\n\nRegards,\nQuick SOS Support'.format(user.first_name, token)
     
     send_mail(
         subject = "RESET PASSWORD OTP FOR QUICK SOS",
         message= message,
         # html_message=msg_html,
         from_email  = 'QUICK SOS <noreply@quicksos.com>',
-        recipient_list= [instance.email]
+        recipient_list= [user.email]
     )
     
     
