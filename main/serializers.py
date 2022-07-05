9663948d33d5c39d2aba53c553975794e80e62af
sirdesmond09 <sirdesmond09@gmail.com>
2022-07-05 22:42:19 +0100

    fix add meessage throug call

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-
-from main.views import agencies
 from .models import Agency, Answer, Issue, Message, Question
 from rest_framework.exceptions import ValidationError
 from django.utils import timezone
