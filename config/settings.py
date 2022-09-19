eed2e3cd87e26ffb6be832fd3a3af06b1f5c5ad6
Nnebue Desmond <46574948+sirdesmond09@users.noreply.github.com>
2022-09-19 15:00:20 +0100

    database config

--- config/settings.py
+++ config/settings.py
@@ -248,4 +248,13 @@ class Production(Staging):
     """
     The in-production settings.
     """
-    pass
+    DATABASES = {  
+        'default': {  
+            'ENGINE': 'django.db.backends.mysql',  
+            'NAME': os.getenv("DB_NAME"),  
+            'USER': os.getenv("DB_USER"),  
+            'PASSWORD': os.getenv("DB_PASSWORD"),  
+            'HOST': os.getenv("DB_HOST"),  
+            'PORT': os.getenv("DB_PORT")
+        }  
+    }  
